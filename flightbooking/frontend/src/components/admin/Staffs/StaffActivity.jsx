15d959ab093a769f2c6a4086f463520bd77719ce
myky034 <vumyky.034@gmail.com>
2023-04-19 21:33:46 +0700

    create staffactivity page

--- flightbooking/frontend/src/components/admin/Staffs/StaffActivity.jsx
+++ flightbooking/frontend/src/components/admin/Staffs/StaffActivity.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const StaffActivity = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <button onClick={handleShow}>Add New Staff</button>
 
-export default StaffActivity
+      {show && (
+        <div
+          className="modal show fade modal-dialog modal-dialog-centered modal-dialog-scrollable"
+          tabindex="-1"
+        >
+          <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div clasNames="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Modal title</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={handleClose}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <p>Modal body text goes here.</p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={handleClose}
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={handleClose}
+                >
+                  Save changes
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StaffActivity;
