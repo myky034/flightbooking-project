c3dfb71ed04e64fa835838a609b589501f11af63
myky034 <vumyky.034@gmail.com>
2023-05-22 09:26:20 +0700

    update db name and password

--- flightbooking/backend/config/connectDB.js
+++ flightbooking/backend/config/connectDB.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("flightbooking_dev", "root", "Khakhavnd@123",
+const sequelize = new Sequelize("flightbooking_dev", "root", "password",
   {
     host: "localhost",
     dialect: "mysql",
